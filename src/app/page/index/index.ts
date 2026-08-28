6bac268b78a78164ece6d5b77ce52edba56d8f61
Bricio-3D <vini.161092@gmail.com>
2026-08-27 23:59:11 -0300

    Implementa validacao de login (admin/123456) e paleta de cores Ford

--- src/app/page/index/index.ts
+++ src/app/page/index/index.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './index.html',
   styleUrl: './index.css',
 })
 export class Index {
+  private router = inject(Router);
+
+  //variaveis conectadas ao fomulario
+  username: string = '';
+  password: string = '';
+
+  //Mensagem de erro
+  errorMessage:string ='';
+
+  //Função para enviar o formulario
+  fazerLogin():void {
+
+    this.errorMessage = '';
+
+    //Validação dos dados solicitados (admin/123456)
+    if (this.username === 'admin' && this.password === '123456'){
+      //Sucesso: vai para Home
+      this.router.navigate(['/home']);
+    } else{
+      //Erro: exibe aviso
+      this.errorMessage = 'Usuario ou senha incorretos';
+    }
+
+
+  }
 
 }
